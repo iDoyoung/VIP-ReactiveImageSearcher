61fa2090711f29ceb14d0f5e3c559de5bdcf7ab6
iDoyoung <ido.young@icloud.com>
2022-06-06 12:05:30 +0900

    feat: Implement Data transfer service

--- ReactiveImageSearcher/ReactiveImageSearcher/Network/DataTransferService.swift
+++ ReactiveImageSearcher/ReactiveImageSearcher/Network/DataTransferService.swift
@@ -14,4 +14,97 @@ enum DataTransferError: Error {
     case resolvedNetworkFailure(Error)
 }
 
+protocol ResponseDecoder {
+    func decode<T: Decodable>(_ data: Data) throws -> T
+}
 
+protocol DataTransferErrorResolvable {
+    func resolve(error: NetworkError) -> Error
+}
+
+protocol DataTransferable {
+    typealias CompletionHandler<T> = (Result<T, DataTransferError>) -> Void
+    
+    func request<T: Decodable, E: ResponseRequestable>(with endpoint: E,
+                                                       completion: @escaping CompletionHandler<T>) where E.Response == T
+    
+    func request<E: ResponseRequestable>(with endpoint: E,
+                                         completion: @escaping CompletionHandler<Void>) where E.Response == Void
+}
+
+class JSONResponseDecoder: ResponseDecoder {
+    private let jsonDecoder = JSONDecoder()
+    init() { }
+    func decode<T: Decodable>(_ data: Data) throws -> T {
+        return try jsonDecoder.decode(T.self, from: data)
+    }
+}
+
+class DataTransferErrorResolver: DataTransferErrorResolvable {
+    public init() { }
+    public func resolve(error: NetworkError) -> Error {
+        return error
+    }
+}
+
+final class DataTransferService: DataTransferable {
+    
+    private let networkService: NetworkServicing
+    private let errorResolver: DataTransferErrorResolvable
+    
+    init(with networkService: NetworkServicing, errorResolver: DataTransferErrorResolvable = DataTransferErrorResolver()) {
+        self.networkService = networkService
+        self.errorResolver = errorResolver
+    }
+    
+    func request<T, E>(with endpoint: E, completion: @escaping CompletionHandler<T>) where T : Decodable, T == E.Response, E : ResponseRequestable {
+        networkService.reqeust(endpoint: endpoint) { result in
+            switch result {
+            case .success(let data):
+                let result: Result<T, DataTransferError> = self.decode(data: data)
+                DispatchQueue.main.async {
+                    completion(result)
+                }
+            case .failure(let error):
+                let error = self.resolve(networkError: error)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+            
+    func request<E>(with endpoint: E, completion: @escaping CompletionHandler<Void>) where E : ResponseRequestable, E.Response == Void {
+        networkService.reqeust(endpoint: endpoint) { result in
+            switch result {
+            case .success:
+                DispatchQueue.main.async {
+                    completion(.success(()))
+                }
+            case .failure(let error):
+                let error = self.resolve(networkError: error)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
+    private func decode<T: Decodable>(data: Data?, decoder: ResponseDecoder = JSONResponseDecoder()) -> Result<T, DataTransferError> {
+        do {
+            guard let data = data else {
+                return .failure(.noResponse)
+            }
+            let result: T = try decoder.decode(data)
+            return .success(result)
+        } catch {
+            return .failure(.parsing(error))
+        }
+    }
+    
+    private func resolve(networkError error: NetworkError) -> DataTransferError {
+        let resolvedError = self.errorResolver.resolve(error: error)
+        return resolvedError is NetworkError ? .networkFailure(error) : .resolvedNetworkFailure(resolvedError)
+    }
+    
+}
