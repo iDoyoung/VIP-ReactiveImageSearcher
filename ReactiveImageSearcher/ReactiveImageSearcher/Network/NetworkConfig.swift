afe7516778991cda657fd70a0c0b6d2cceab62f0
iDoyoung <ido.young@icloud.com>
2022-06-19 21:50:42 +0900

    feat: Add NetworkConfiguration

--- ReactiveImageSearcher/ReactiveImageSearcher/Network/NetworkConfig.swift
+++ ReactiveImageSearcher/ReactiveImageSearcher/Network/NetworkConfig.swift
@@ -12,3 +12,17 @@ protocol NetworkConfigurable {
     var headers: [String: String] { get }
     var queryParameters: [String: String] { get }
 }
+
+struct NetworkConfiguration: NetworkConfigurable {
+    let baseURL: URL
+    let headers: [String: String]
+    let queryParameters: [String: String]
+    
+    init(baseURL: URL,
+         headers: [String: String] = [:],
+         queryParameters: [String: String] = [:]) {
+        self.baseURL = baseURL
+        self.headers = headers
+        self.queryParameters = queryParameters
+    }
+}
