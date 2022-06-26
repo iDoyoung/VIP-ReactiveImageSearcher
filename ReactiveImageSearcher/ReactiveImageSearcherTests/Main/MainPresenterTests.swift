473132b59125395ee4dfa2c059cafc0e49804d83
iDoyoung <ido.young@icloud.com>
2022-06-26 11:35:46 +0900

    Test MainPresenter Logic

---
+++ ReactiveImageSearcher/ReactiveImageSearcherTests/Main/MainPresenterTests.swift
@@ -0,0 +1,63 @@
+//
+//  MainPresenterTests.swift
+//  ReactiveImageSearcherTests
+//
+//  Created by Doyoung on 2022/06/26.
+//
+
+import XCTest
+@testable import ReactiveImageSearcher
+
+class MainPresenterTests: XCTestCase {
+
+    //MARK: - System under test
+    var sut: MainPresenter!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = MainPresenter()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+    
+    //MARK: - Test doubles
+    class MainDisplayLogicSpy: MainDisplayLogic {
+        
+        var displayRandomPhotoCalled = false
+        var displayFailureFetchingAlertCalled = false
+        
+        func displayRandomPhoto() {
+            displayRandomPhotoCalled = true
+        }
+        
+        func displayFailureFetchingAlert() {
+            displayFailureFetchingAlertCalled = true
+        }
+        
+    }
+    
+    //MARK: - TestS
+    func test_shouldCallDisplayRandomPhotoWhenShowSuccessFetchedRandomPhoto() {
+        //Given
+        let mainDisplayLogic = MainDisplayLogicSpy()
+        sut.viewController = mainDisplayLogic
+        //When
+        sut.showSuccessFetchedRandomPhoto()
+        //Then
+        XCTAssert(mainDisplayLogic.displayRandomPhotoCalled, "Display random photo")
+    }
+    
+    func test_shouldCallDisplayFailureFetchingAlertWhenShowFailureFetchedRandomPhoto() {
+        //Given
+        let mainDisplayLogic = MainDisplayLogicSpy()
+        sut.viewController = mainDisplayLogic
+        //When
+        sut.showFailureFetchedRandomPhoto()
+        //Then
+        XCTAssert(mainDisplayLogic.displayFailureFetchingAlertCalled, "Display Failure Fetching Alert")
+    }
+    
+}
