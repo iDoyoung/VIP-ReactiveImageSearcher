968122edd71a3488ee968f5f9d40d369f6796b8e
iDoyoung <ido.young@icloud.com>
2022-06-26 18:28:21 +0900

    Test fetch random photo when Vew will appear in Main View Controller

---
+++ ReactiveImageSearcher/ReactiveImageSearcherTests/Main/MainViewControllerTests.swift
@@ -0,0 +1,62 @@
+//
+//  MainViewControllerTests.swift
+//  ReactiveImageSearcherTests
+//
+//  Created by Doyoung on 2022/06/26.
+//
+
+import XCTest
+@testable import ReactiveImageSearcher
+
+class MainViewControllerTests: XCTestCase {
+
+    //MARK: - System under test
+    var sut: MainViewController!
+    var window: UIWindow!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        window = UIWindow()
+        setUpViewController()
+    }
+
+    override func tearDownWithError() throws {
+        window = nil
+        sut = nil
+        try super.tearDownWithError()
+    }
+    
+    func setUpViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        sut = storyboard.instantiateViewController(withIdentifier: "MainViewController") as? MainViewController
+    }
+
+    
+    func loadView() {
+      window.addSubview(sut.view)
+    }
+    
+    //MARK: - Test doubles
+    class MainBusinessLogicSpy: MainBusinessLogic {
+        
+        var fetchRandomPhotoCalled = false
+        
+        func fetchRandomPhoto() {
+            fetchRandomPhotoCalled = true
+        }
+        
+    }
+    
+    //MARK: Tests
+    func test_shouldDisplayFetchedRandomPhotoWhenViewWillAppear() {
+        //Given
+        let mainBussiendLogic = MainBusinessLogicSpy()
+        sut.interactor = mainBussiendLogic
+        loadView()
+        //When
+        sut.viewWillAppear(true)
+        //Then
+        XCTAssert(mainBussiendLogic.fetchRandomPhotoCalled, "Should fetch random photo right after the view will appear")
+    }
+    
+}
