1a89d7407a239b67b426754bde1580410feae84b
iDoyoung <ido.young@icloud.com>
2022-06-28 10:12:12 +0900

    Initial project

--- ReactiveImageSearcher+RxSwift/ReactiveImageSearcher+RxSwift/MainViewController.swift
+++ ReactiveImageSearcher+RxSwift/ReactiveImageSearcher+RxSwift/MainViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,4 +18,3 @@ class ViewController: UIViewController {
 
 
 }
-
