8eba7ca519301b4613082cfe33e4cde38a454726
iDoyoung <ido.young@icloud.com>
2022-07-01 13:54:40 +0900

    Implement Main view controller

---
+++ ReactiveImageSearcher+RxSwift/ReactiveImageSearcher+RxSwift/Scenes/MainViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  ReactiveImageSearcher+RxSwift
+//
+//  Created by Doyoung on 2022/05/27.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    var interactor: MainBusinessLogic?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //MARK: - VC Life cycle
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setUpViewController()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpViewController()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func setupViewController() {
+        let viewController = self
+        let interactor = MainInteractor()
+        let presenter = MainPresenter()
+        viewController.interactor = interactor
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+    }
+
+}
+
+extension MainViewController: MainDisplayLogic{
+    
+    func displayRandomPhoto(_ photo: Photo) {
+        imageView.loadImage(url: photo.urls.full)
+    }
+    
+    func displayFailureFetchingAlert() {
+        print("Error")
+    }
+    
+}
+
+protocol MainDisplayLogic {
+    func displatRandomPhoto(_ photo: Photo)
+    func displayFailureFetchingAlert()
+}
