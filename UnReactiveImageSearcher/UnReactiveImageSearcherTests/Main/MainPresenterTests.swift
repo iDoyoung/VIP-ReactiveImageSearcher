bbf1187326c83483dd5bac678bace6323b36df24
iDoyoung <ido.young@icloud.com>
2022-07-03 19:37:50 +0900

    Rename project

--- UnReactiveImageSearcher/UnReactiveImageSearcherTests/Main/MainPresenterTests.swift
+++ UnReactiveImageSearcher/UnReactiveImageSearcherTests/Main/MainPresenterTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import ReactiveImageSearcher
+@testable import UnReactiveImageSearcher
 
 class MainPresenterTests: XCTestCase {
 
--- UnReactiveImageSearcher/UnReactiveImageSearcherTests/Main/MainViewControllerTests.swift
+++ UnReactiveImageSearcher/UnReactiveImageSearcherTests/Main/MainViewControllerTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import ReactiveImageSearcher
+@testable import UnReactiveImageSearcher
 
 class MainViewControllerTests: XCTestCase {
 
